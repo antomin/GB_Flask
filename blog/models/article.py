48ff4b455c1ec8a17cce10418ccf683cb97b8a4f
Anton Mindlin <37115351+antomin@users.noreply.github.com>
2023-03-22 13:26:57 +0200

    Lesson 7 ready. (#9)

--- blog/models/article.py
+++ blog/models/article.py
@@ -1,8 +1,16 @@
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Text
+from sqlalchemy import (Column, DateTime, ForeignKey, Integer, String, Table,
+                        Text)
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 
 from blog.extensions import db
+
+article_tag_association_table = Table(
+    'article_tag_association',
+    db.metadata,
+    Column('article_id', Integer, ForeignKey('article.id'), nullable=False),
+    Column('tag_id', Integer, ForeignKey('tag.id'), nullable=False)
+)
 
 
 class Article(db.Model):
@@ -14,3 +22,4 @@ class Article(db.Model):
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
 
     author = relationship('Author', back_populates='articles')
+    tags = relationship('Tag', secondary=article_tag_association_table, back_populates='articles')
---
+++ blog/models/tag.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from blog.extensions import db
+from blog.models.article import article_tag_association_table
+
+
+class Tag(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100), nullable=False)
+
+    articles = relationship('Article', secondary=article_tag_association_table, back_populates='tags')
