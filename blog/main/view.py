3b64a416b07a31619115c2d8a71902645f179fc8
Anton Mindlin <anton.mindlin@gmail.com>
2023-04-11 17:15:57 +0200

    db

--- blog/main/view.py
+++ blog/main/view.py
@@ -9,7 +9,7 @@ main_app = Blueprint('main_app', __name__, static_folder='static')
 @main_app.route('/', endpoint='index')
 def index():
     # new_articles = Article.query.order_by('created_at').limit(3)
-    response = requests.get('http://127.0.0.1:5000/api/articles/')
+    response = requests.get('https://geekblog.onrender.com//api/articles/')
     json_articles = response.json()
     new_articles = json_articles.get('data')
     return render_template('main/index.html', new_articles=new_articles)
